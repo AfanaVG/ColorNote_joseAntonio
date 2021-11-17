444af3287ca59ed5b5fd675bcf4313de5bc3a48f
AfanaVG <manteropbdbrujas@gmail.com>
2021-11-17 17:35:05 +0100

    Auto stash before revert of "Fuente añadida"

--- ColorNote_joseAntonio/app/src/main/java/com/example/colornote_joseantonio/MainActivity.kt
+++ ColorNote_joseAntonio/app/src/main/java/com/example/colornote_joseantonio/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.colornote_joseantonio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var pepe = arrayListOf<String>("pepe","holi")
+
+        //var ml: ListView = findViewById(R.id.listVNotas_MainActivity)
+
+
     }
 }
