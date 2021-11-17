6e1f7eee5e6bb63ef6b8a439ea9feee29e2d914a
AfanaVG <manteropbdbrujas@gmail.com>
2021-11-17 20:39:22 +0100

    Implementado RecyclerView

---
+++ ColorNote_joseAntonio/app/src/main/java/com/example/colornote_joseantonio/Adaptadores/MiAdaptadorRecycler.kt
@@ -0,0 +1,127 @@
+package com.example.colornote_joseantonio.Adaptadores
+
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.colornote_joseantonio.Model.Nota
+import com.example.colornote_joseantonio.R
+
+
+class MiAdaptadorRecycler(var notas : ArrayList<Nota>, var  context: Context) : RecyclerView.Adapter<MiAdaptadorRecycler.ViewHolder>() {
+
+    companion object {
+        //Esta variable estática nos será muy útil para saber cual está marcado o no.
+        var seleccionado:Int = -1
+        /*
+        PAra marcar o desmarcar un elemento de la lista lo haremos diferente a una listView. En la listView el listener
+        está en la activity por lo que podemos controlar desde fuera el valor de seleccionado y pasarlo al adapter, asociamos
+        el adapter a la listview y resuelto.
+        En las RecyclerView usamos para pintar cada elemento la función bind (ver código más abajo, en la clase ViewHolder).
+        Esto se carga una vez, solo una vez, de ahí la eficiencia de las RecyclerView. Si queremos que el click que hagamos
+        se vea reflejado debemos recargar la lista, para ello forzamos la recarga con el método: notifyDataSetChanged().
+         */
+    }
+
+
+
+
+
+
+
+
+
+    /**
+     * onBindViewHolder() se encarga de coger cada una de las posiciones de la lista de personajes y pasarlas a la clase
+     * ViewHolder(clase interna, ver abajo) para que esta pinte todos los valores y active el evento onClick en cada uno.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = notas.get(position)
+        holder.bind(item, context, position, this)
+    }
+
+    /**
+     *  Como su nombre indica lo que hará será devolvernos un objeto ViewHolder al cual le pasamos la celda que hemos creado.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        //return ViewHolder(layoutInflater.inflate(R.layout.item_lo,parent,false))
+        return ViewHolder(layoutInflater.inflate(R.layout.item_nota_card,parent,false))
+    }
+
+    /**
+     * getItemCount() nos devuelve el tamaño de la lista, que lo necesita el RecyclerView.
+     */
+    override fun getItemCount(): Int {
+
+        return notas.size
+    }
+
+
+    //--------------------------------- Clase interna ViewHolder -----------------------------------
+    /**
+     * La clase ViewHolder. No es necesaria hacerla dentro del adapter, pero como van tan ligadas
+     * se puede declarar aquí.
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        //Esto solo se asocia la primera vez que se llama a la clase, en el método onCreate de la clase que contiene a esta.
+        //Por eso no hace falta que hagamos lo que hacíamos en el método getView de los adaptadores para las listsViews.
+        //val nombrePersonaje = view.findViewById(R.id.txtNombre) as TextView
+        //val tipoPersonaje = view.findViewById(R.id.txtTipo) as TextView
+        //val avatar = view.findViewById(R.id.imgImagen) as ImageView
+
+        //Como en el ejemplo general de las listas (ProbandoListas) vemos que se puede inflar cada elemento con una card o con un layout.
+        val nombreNota = view.findViewById(R.id.txtNombre_itemNota) as TextView
+
+
+
+
+        /**
+         * Éste método se llama desde el método onBindViewHolder de la clase contenedora. Como no vuelve a crear un objeto
+         * sino que usa el ya creado en onCreateViewHolder, las asociaciones findViewById no vuelven a hacerse y es más eficiente.
+         */
+        fun bind(nota: Nota, context: Context, pos: Int, miAdaptadorRecycler: MiAdaptadorRecycler){
+            nombreNota.text = nota.nombre
+
+
+/**
+            when(encuesta.sistemaOperativo){
+                "LINUX"-> Glide.with(context).load(R.drawable.linux).into(so)
+                "WINDOWS"-> Glide.with(context).load(R.drawable.windows).into(so)
+                "MAC"-> Glide.with(context).load(R.drawable.apple).into(so)
+            }**/
+
+            //Para marcar o desmarcar al seleccionado usamos el siguiente código.
+            if (pos == MiAdaptadorRecycler.seleccionado) {
+                with(nombreNota) {
+                    this.setTextColor(resources.getColor(R.color.purple_200))
+                }
+            }
+            else {
+                with(nombreNota) {
+                    this.setTextColor(resources.getColor(R.color.black))
+                }
+            }
+            //Se levanta una escucha para cada item. Si pulsamos el seleccionado pondremos la selección a -1, en otro caso será el nuevo sleccionado.
+            itemView.setOnClickListener(View.OnClickListener
+                    {
+                        if (pos == MiAdaptadorRecycler.seleccionado){
+                            MiAdaptadorRecycler.seleccionado = -1
+                        }
+                        else {
+                            MiAdaptadorRecycler.seleccionado = pos
+
+                        }
+                        //Con la siguiente instrucción forzamos a recargar el viewHolder porque han cambiado los datos. Así pintará al seleccionado.
+                        miAdaptadorRecycler.notifyDataSetChanged()
+
+                        Toast.makeText(context, "Valor seleccionado " +  MiAdaptadorRecycler.seleccionado.toString(), Toast.LENGTH_SHORT).show()
+                    })
+        }
+    }
+}
+
--- ColorNote_joseAntonio/app/src/main/java/com/example/colornote_joseantonio/MainActivity.kt
+++ ColorNote_joseAntonio/app/src/main/java/com/example/colornote_joseantonio/MainActivity.kt
@@ -4,14 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.widget.ListView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.colornote_joseantonio.Adaptadores.MiAdaptadorRecycler
+import com.example.colornote_joseantonio.Model.Nota
+import java.sql.Date
+import java.sql.Time
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var miRecyclerView : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        var pepe = arrayListOf<String>("pepe","holi")
+        var pepe = arrayListOf<Nota>(Nota("hola","simple", Time(10,10,10), Date(1999,10,1)))
+        pepe.add(Nota("hola","simple", Time(10,10,10), Date(1999,10,1)))
+        pepe.add(Nota("hola","simple", Time(10,10,10), Date(1999,10,1)))
+        pepe.add(Nota("hola","simple", Time(10,10,10), Date(1999,10,1)))
+        pepe.add(Nota("hola","simple", Time(10,10,10), Date(1999,10,1)))
+        pepe.add(Nota("hola","simple", Time(10,10,10), Date(1999,10,1)))
+        pepe.add(Nota("hola","simple", Time(10,10,10), Date(1999,10,1)))
+        pepe.add(Nota("hola","simple", Time(10,10,10), Date(1999,10,1)))
+
+
+        miRecyclerView = findViewById(R.id.listaNotas_mainActivity) as RecyclerView
+        miRecyclerView.setHasFixedSize(true)
+        miRecyclerView.layoutManager = LinearLayoutManager(this)
+        var miAdapter = MiAdaptadorRecycler(pepe, this)
+        miRecyclerView.adapter = miAdapter
 
         //var ml: ListView = findViewById(R.id.listVNotas_MainActivity)
 
